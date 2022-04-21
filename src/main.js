2eae756c6de82fbe5fd8badb41f846abb08be257
Nelson Pham <nelsonpham@cma-100-64-25-159.resnet.ucsc.edu>
2022-04-21 12:48:06 -0700

    Modified main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Testing git push - Ernani
 let config = {
     type: Phaser.CANVAS,
@@ -22,32 +21,3 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
 let keyUP, keyDOWN, keyLEFT, keyRIGHT;
-=======
-let config = {
-    type: Phaser.WEBGL,
-    width: 640,
-    height: 480,
-    scene: [ Menu, LevelOne,LevelTwo ],
-    render: {
-      pixelArt: true
-    },
-    physics:{
-      default:'arcade',
-      arcade: {
-        debug: false,
-        debugShowBody: true,
-        gravity:{
-          x: 0,
-          y: 0,
-        }
-      }
-    },
-    
-  }
-  let game = new Phaser.Game(config);
-  // set UI sizes
-  let borderUISize = game.config.height / 15;
-  let borderPadding = borderUISize / 3;
-  // reserve keyboard vars
-  let keyF, keyR, keyLEFT, keyRIGHT;
->>>>>>> 2101a919a1832f07a47ff3b3e2b6d8a474108bf9
