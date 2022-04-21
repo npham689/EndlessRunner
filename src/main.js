d75ab41a1b729652a5fb0f1b518035d278d3a797
Nelson Pham <nelsonpham@cma-100-64-25-159.resnet.ucsc.edu>
2022-04-21 12:40:00 -0700

    Initialized Physics Arcade library for use

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 // Testing git push - Ernani
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200},
+            debug: true
+        }
+    },
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+// reserve keyboard variables
+let keyUP, keyDOWN, keyLEFT, keyRIGHT;
