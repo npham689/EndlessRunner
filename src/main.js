ad929ee0d722c48affab8aa09bbfac17d16e394c
Grant Bosworth <gboswort@ucsc.edu>
2022-04-21 12:34:37 -0700

    skeleton

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-// Testing git push - Ernani
+let config = {
+    type: Phaser.WEBGL,
+    width: 640,
+    height: 480,
+    scene: [ Menu, LevelOne,LevelTwo ],
+    render: {
+      pixelArt: true
+    },
+    physics:{
+      default:'arcade',
+      arcade: {
+        debug: false,
+        debugShowBody: true,
+        gravity:{
+          x: 0,
+          y: 0,
+        }
+      }
+    },
+    
+  }
+  let game = new Phaser.Game(config);
+  // set UI sizes
+  let borderUISize = game.config.height / 15;
+  let borderPadding = borderUISize / 3;
+  // reserve keyboard vars
+  let keyF, keyR, keyLEFT, keyRIGHT;
