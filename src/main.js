eeae546226d54e27701c7eb6ce5ed76fb1350360
Grant Bosworth <gboswort@ucsc.edu>
2022-04-22 14:50:15 -0700

    adding basics

--- src/main.js
+++ src/main.js
@@ -1,23 +1,41 @@
-let config = {
-    type: Phaser.CANVAS,
-    width: 640,
-    height: 480,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200},
-            debug: true
-        }
-    },
-    scene: [ Menu, Play ]
+
+// global game options
+let gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+    playerGravity: 900,
+    jumpForce: 400,
+    playerStartPosition: 200,
+    jumps: 2
 }
+window.onload = function() {
+    let config = {
+        type: Phaser.AUTO,
+        width: 1334,
+        height: 750,
+        scene: playGame,
+        backgroundColor: 0x444444,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200},
+                debug: true
+            }
+        },
+        scene: [ Menu, Play ]
+    }
+    let game = new Phaser.Game(config);
 
-let game = new Phaser.Game(config);
+    // set UI sizes
+    let borderUISize = game.config.height / 15;
+    let borderPadding = borderUISize / 3;
 
-// set UI sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
-
-// reserve keyboard variables
-let keyUP, keyDOWN, keyLEFT, keyRIGHT;
-//test
+    // reserve keyboard variables
+    let keyUP, keyDOWN, keyLEFT, keyRIGHT;
+    //test
+    window.focus();
+    resize();
+    window.addEventListener("resize", resize, false);
+}
+ 
