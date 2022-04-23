bdc048f89f050b1fcb9ec3e6e8fb56de8f7b4922
Nelson Pham <nelsonpham@Nelsons-MacBook-Pro.local>
2022-04-23 00:41:00 -0700

    Added background image and Menu text and modified main.js a bit

--- src/main.js
+++ src/main.js
@@ -9,13 +9,14 @@ let gameOptions = {
     playerStartPosition: 200,
     jumps: 2
 }
+
 window.onload = function() {
     let config = {
         type: Phaser.AUTO,
         width: 1334,
         height: 750,
         scene: [ Menu, Play ],
-        backgroundColor: 0x444444,
+        backgroundColor: 0x4FFFFE0,
         physics: {
             default: 'arcade',
             arcade: {
@@ -26,30 +27,28 @@ window.onload = function() {
     }
     let game = new Phaser.Game(config);
 
-    // set UI sizes
-    let borderUISize = game.config.height / 15;
-    let borderPadding = borderUISize / 3;
-
-    // reserve keyboard variables
-    let keyUP, keyDOWN, keyLEFT, keyRIGHT;
     //test
     window.focus();
     resize();
     window.addEventListener("resize", resize, false);
-    function resize(){
-        let canvas = document.querySelector("canvas");
-        let windowWidth = window.innerWidth;
-        let windowHeight = window.innerHeight;
-        let windowRatio = windowWidth / windowHeight;
-        let gameRatio = game.config.width / game.config.height;
-        if(windowRatio < gameRatio){
-            canvas.style.width = windowWidth + "px";
-            canvas.style.height = (windowWidth / gameRatio) + "px";
-        }
-        else{
-            canvas.style.width = (windowHeight * gameRatio) + "px";
-            canvas.style.height = windowHeight + "px";
-        }
+}
+
+// reserve keyboard variables
+let keyUP, keyDOWN, keyLEFT, keyRIGHT;
+
+function resize(){
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth / windowHeight;
+    let gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
     }
 }
 
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,20 +1,20 @@
 class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
-    }
-    //this.scene.start("playScene");  
+    } 
     
     preload() {
-    // load audio
+      // load audio/background
+      this.load.image('background', './assets/background.png');
     }
 
     create() {
         // menu text configuration
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            fontFamily: 'Georgia',
+            fontSize: '38px',
+            //backgroundColor: '#000000',
+            color: '#0000FF',
             align: 'right',
             padding: {
                 top: 5,
@@ -23,11 +23,12 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
         // show menu text
-        //this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'WELCOME TO THE WILD WILD WEST', menuConfig).setOrigin(0.5);
+        this.add.image(250, 150, 'background');
+        this.add.text(410, 300, 'WELCOME TO THE LOUD QUIET', menuConfig).setOrigin(0.5);
         //this.add.text(game.config.width/2, game.config.height/2, 'P1 rides with ←→ arrows & (F) to fire', menuConfig).setOrigin(0.5);
         //this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'P2 drags the mouse', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
+        //menuConfig.backgroundColor = '#00FF00';
+        //menuConfig.color = '#000';
         //this.add.text(game.config.width/2, game.config.height/2 + borderUISize *2 + borderUISize*2 , 'Press ← for Farmhand or → for Cowboy', menuConfig).setOrigin(0.5);
   
         // define keys
